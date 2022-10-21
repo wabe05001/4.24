90aebb6485216d70f6d71219d9310d3be0341976
TheBlueSoul5188 <wabe05001@utb.vaxjo.se>
2022-10-21 10:13:08 +0200

    Add project files.

---
+++ 4.24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_4._20
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+
+}
